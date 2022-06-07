68a51b54c881a351db9f7f95148e888281073909
Constantine Yannuk <cyannuk@issart.com>
2022-06-07 16:53:13 +0000

    Fix gnosis update

--- updates/sql/gnosis/update.sql
+++ updates/sql/gnosis/update.sql
@@ -1,8 +1,8 @@
 -- DELETE FROM gnosis_protocol_v2.trades WHERE block_time >= (SELECT DATE_TRUNC('day', now()) - INTERVAL '3 months');
-SELECT gnosis_protocol_v2.insert_trades((SELECT max(block_time) - interval '6 hours' FROM gnosis_protocol_v2.trades))
+SELECT gnosis_protocol_v2.insert_trades((SELECT max(block_time) - interval '6 hours' FROM gnosis_protocol_v2.trades)) ;
 
 --DELETE FROM gnosis_protocol_v2.batches WHERE block_time >= (SELECT DATE_TRUNC('day', now()) - INTERVAL '6 months');
-SELECT gnosis_protocol_v2.insert_batches((SELECT max(block_time) - interval '6 hours' FROM gnosis_protocol_v2.batches))
+SELECT gnosis_protocol_v2.insert_batches((SELECT max(block_time) - interval '6 hours' FROM gnosis_protocol_v2.batches)) ;
 
 REFRESH MATERIALIZED VIEW CONCURRENTLY gnosis_protocol.view_tokens ;
 
